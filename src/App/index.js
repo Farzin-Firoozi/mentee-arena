36e7e84f8f70f4e2a41c9843b5b027602a67770e
zeinabsayyadi <zeinsa0158@gmail.com>
2023-06-01 17:54:07 +0430

    up : set up routs

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,31 @@
 import Characters from "../pages/Characters";
 import Layout from "../layout";
+import { Route, Routes } from "react-router-dom";
+import SearchCharacters from "../pages/SearchCharacters";
+import Bookmarks from "../pages/Bookmarks";
 
 function App() {
-  
   return (
     <main>
       <Layout>
-        <Characters />
+        <Routes>
+          <Route
+            path="/"
+            element={<Characters />}
+          />
+          <Route
+            path="/search-character"
+            element={<SearchCharacters />}
+          />
+          <Route
+            path="/bookmark"
+            element={<Bookmarks />}
+          />
+          <Route
+            path="/*"
+            element={<p>page not found</p>}
+          />
+        </Routes>
       </Layout>
     </main>
   );
